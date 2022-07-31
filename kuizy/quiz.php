472fa7c20e3fd66db8c7d44a31a40515cd15bfef
keishiro terashita <20153180g@gmail.com>
2022-07-31 20:02:30 +0900

    week22,23完了

--- kuizy/quiz.php
+++ kuizy/quiz.php
@@ -6,14 +6,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php 
-$id = filter_input(INPUT_GET, "id");
-if ($id === "1"){ ?>
-  <title>東京の難読地名クイズ</title>
-  <?php }
-if ($id === "2"){ ?>
-<title>広島県の難読地名クイズ</title>
-<?php } ?>
 
+try{
+  $id = (int)filter_input(INPUT_GET, "id");
+  //データベースに接続
+  $pdo = new PDO (
+    "mysql:host=db;dbname=posse;charset=utf8mb4",
+    "root",
+    "root"
+  );
+  // $stmt = $pdo->query("SHOW TABLES FROM posse");
+  $stmt = $pdo->prepare("SELECT name from posse.big_questions WHERE id = :n");
+  $stmt->execute(["n"=>(int)$id]);
+  $title = $stmt->fetch();
+}catch(PDOException $e) {
+  echo $e->getMessage();
+}
+?>
+  <title>
+    <?= $title["name"] ?>
+  </title>
+  
 </head>
 <body>
 </body>
