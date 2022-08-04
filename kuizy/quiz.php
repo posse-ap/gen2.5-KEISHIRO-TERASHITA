57260a1445709d69a6c4f97330a9378b3f330f4d
keishiro terashita <20153180g@gmail.com>
2022-08-04 22:29:49 +0900

    init.sql でデータベースを書き直した。phpの書き方を少しだけ直した。

--- kuizy/quiz.php
+++ kuizy/quiz.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <?php
-
   try {
     //クエリから問題番号を取得
     $id = (int)filter_input(INPUT_GET, "id");
@@ -16,7 +7,7 @@
     $pdo = new PDO(
       "mysql:host=db;dbname=posse;charset=utf8mb4",
       "root",
-      "root"
+      "password"
     );
 
     // タイトルを取得
@@ -29,55 +20,60 @@
     $stmt->execute([":big_question_id" => $id]);
     $questions = $stmt->fetchAll();
     $questions_length = count($questions);
-
   } catch (PDOException $e) {
     echo $e->getMessage();
   }
   ?>
-  <link rel="stylesheet" href="./style/reset.css">
-  <link rel="stylesheet" href="./style/style.css">
-  <title>
-    <?= $title["name"] ?>
-  </title>
-</head>
 
-<body>
+  <!DOCTYPE html>
+  <html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <article>
-    <?php
-    // 一問ずつ作成
-    for ($question_num = 0; $question_num < $questions_length; $question_num++) :
+    <link rel="stylesheet" href="./style/reset.css">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>
+      <?= $title["name"] ?>
+    </title>
+  </head>
 
-      // 選択肢の取得
-      $stmt = $pdo->prepare("SELECT * FROM posse.choices WHERE question_id = :question_num");
-      $stmt->execute([":question_num" => $questions[$question_num]["id"]]);
-      $choices = $stmt->fetchAll();
+  <body>
+    <article>
+      <?php
+      // 一問ずつ作成
+      for ($question_num = 0; $question_num < $questions_length; $question_num++) :
+
+        // 選択肢の取得
+        $stmt = $pdo->prepare("SELECT * FROM posse.choices WHERE question_id = :question_num");
+        $stmt->execute([":question_num" => $questions[$question_num]["id"]]);
+        $choices = $stmt->fetchAll();
       ?>
-      <section>
-        <h2><?= $question_num + 1 ?>.この地名はなんて読む？</h2>
-        <img src="./img/<?= $questions[$question_num]["image"] ?>" alt="">
-        <ul>
-          <?php
-          // ランダムな数列の生成
-          $random = [0,1,2];
-          for($i = 2; $i > 0; $i --):
-            $r = rand(0,$i);
-            $tmp = $random[$i];
-            $random[$i] = $random[$r];
-            $random[$r] = $tmp;
-          endfor;
+        <section>
+          <h2><?= $question_num + 1 ?>.この地名はなんて読む？</h2>
+          <img src="./img/<?= $questions[$question_num]["image"] ?>" alt="">
+          <ul>
+            <?php
+            // ランダムな数列の生成
+            $random = [0, 1, 2];
+            for ($i = 2; $i > 0; $i--) :
+              $r = rand(0, $i);
+              $tmp = $random[$i];
+              $random[$i] = $random[$r];
+              $random[$r] = $tmp;
+            endfor;
 
-          // 選択肢を表示
-          for($l = 0; $l < 3; $l++):
-          ?>
-          <li id = "choice_<?= $question_num ?>_<?= $l ?>_<?= $choices[$random[$l]]["valid"] ?>">
-            <?= $choices[$random[$l]]["name"] ?>
-          </li>
-          <?php endfor; ?>
-        </ul>
-      </section>
-    <?php endfor; ?>
-  </article>
-</body>
-
-</html>
+            // 選択肢を表示
+            for ($l = 0; $l < 3; $l++) :
+            ?>
+              <li id="choice_<?= $question_num ?>_<?= $l ?>_<?= $choices[$random[$l]]["valid"] ?>">
+                <?= $choices[$random[$l]]["name"] ?>
+              </li>
+            <?php endfor; ?>
+          </ul>
+        </section>
+      <?php endfor; ?>
+    </article>
+  </body>
+  </html>
